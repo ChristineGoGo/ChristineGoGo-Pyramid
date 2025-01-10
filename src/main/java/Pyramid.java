e390144cebb1a46f3860a136bfb7e8d8638d1a35
Christine Gogo <brandywang22@gmail.com>
2025-01-10 10:19:12 +0000

    Pyramid challenge

--- src/main/java/Pyramid.java
+++ src/main/java/Pyramid.java
@@ -28,6 +28,14 @@ public class Pyramid {
      * @return a string representation of the pyramid.
      */
     public String returnPyramid(int n){
-        return "";
+        // return "";
+        String res = "";
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                res += "*";
+            }
+            res += "\n";
+        }
+        return res;
     }
 }
